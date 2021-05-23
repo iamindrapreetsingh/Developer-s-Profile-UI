9825c3b140bb571242c22494ba0b88e92e2ef4a4
Indrapreet Singh <indrapreetsingh41@gmail.com>
2021-05-24 03:34:20 +0530

    Add responsiveness.

--- src/header.js
+++ src/header.js
@@ -4,11 +4,15 @@ import "./index.css";
 function GetHeader() {
   return (
     <header className="header">
+      <div className="header-content">
       <div id="header-text">
         <div>The Developer</div>
         <div>Repository</div>
       </div>
-      <img src="header-image.png" alt="header-img" />
+      <div id="header-img-div">
+      <img id="header-img" src="header-image.png" alt="header-img" />
+      </div>
+      </div>
     </header>
   );
 }
