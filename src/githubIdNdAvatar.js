22459ab3374c7d9dd9f22ca498a812ca2830f2f3
Indrapreet Singh <indrapreetsingh41@gmail.com>
2021-06-07 02:28:24 +0530

    FIx alignments and responsiveness issues.

--- src/githubIdNdAvatar.js
+++ src/githubIdNdAvatar.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import "./index.css";
 
-function GetGithubIdNdAvatar(props) {
-  let { githubId, avatarUrl } = props;
-  return (
-    <div className="github-id-nd-avatar">
-      <img id="avatar" alt={avatarUrl} src={avatarUrl} />
-      <span id="github-id">{githubId}</span>
-      <span id="north-east-icon">&#8599;</span>
-    </div>
-  );
+class GetGithubIdNdAvatar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  fetchDeveloperDetails = () => {
+    this.props.fetchDeveloperDetails(this.props.githubId);
+  };
+
+  async componentDidMount() {}
+
+  componentDidUpdate(a, b) {}
+
+  render() {
+    return (
+      <div onClick={this.fetchDeveloperDetails} className="github-id-nd-avatar">
+        <img
+          id="avatar"
+          alt={this.props.avatarUrl}
+          src={this.props.avatarUrl}
+        />
+        <span id="github-id">{this.props.githubId}</span>
+        <span id="north-east-icon">&#8599;</span>
+      </div>
+    );
+  }
 }
 
 export default GetGithubIdNdAvatar;
--- src/githubRepoInfo.js
+++ src/githubRepoInfo.js
@@ -3,14 +3,18 @@ import GetDivider from "./divider";
 import "./index.css";
 
 function GetGithubRepoInfo(props) {
+  const { name, html_url, description, updated_at } = props.repo;
+
   return (
     <div>
       <div className="repo-name-and-desc">
-        <span id="repo-name">Repository Name</span>
+        <a href={html_url} id="repo-link">
+          <span id="repo-name">{name}</span>
+        </a>
         <span id="repo-north-arrow">&#8599;</span>
-        <span id="repo-created-at">Repo Created At</span>
+        <span id="repo-created-at">{updated_at}</span>
       </div>
-      <div id="repo-desc">This is the repo description</div>
+      <div id="repo-desc">{description}</div>
       <GetDivider width="30px" marginTop="10px"></GetDivider>
     </div>
   );
